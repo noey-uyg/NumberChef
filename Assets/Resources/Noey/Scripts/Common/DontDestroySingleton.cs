bc4407bf22eaa1a50230ada3325b22d975a3e367
Noeyuyg <dnlrbdus31@gmail.com>
2025-04-22 17:06:29 +0900

    - Add : 싱글톤

---
+++ Assets/Resources/Noey/Scripts/Common/DontDestroySingleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DontDestroySingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+            OnAwake();
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    protected virtual void OnAwake() { }
+}
---
+++ Assets/Resources/Noey/Scripts/Common/Singleton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            OnAwake();
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    protected virtual void OnAwake() { }
+}
